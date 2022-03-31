e732c7aec49a262bc33ede2b7b17301db6c6087b
Celyn Walters <celyn.walters@surrey.ac.uk>
2022-03-31 10:50:35 +0100

    Add pip install script

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+	name="rl",
+	# version="0.0.1",
+	author="Celyn Walters",
+	author_email="celyn.walters@surrey.ac.uk",
+	url="https://github.com/celynw/rl",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent",
+	],
+	python_requires=">=3.10",
+)
