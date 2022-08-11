3cd71693703de8f2cf72457ac88f399cdf7dd114
Celyn Walters <celyn.walters@surrey.ac.uk>
2022-08-11 19:39:12 +0100

    Develop old models

--- rl/models/dqn.py
+++ rl/models/dqn.py
@@ -1,20 +1,25 @@
 import argparse
+from multiprocessing.sharedctypes import Value
 from typing import Optional
 from collections import deque
+import time
 
 import torch
 from torch.utils.data import DataLoader
 import gym
 import optuna
+from rich import print, inspect
+import numpy as np
 
 from rl.models import Base
 from rl.models.utils import Agent
+from rl.models.utils import ModelType
 from rl.datasets.utils import ReplayBuffer
-from rl.datasets import RL
+from rl.datasets import RLOffPolicy
 from rl.utils import Step, Dir
 
 # ==================================================================================================
-class DQN_net(torch.nn.Module):
+class Discrete(torch.nn.Module):
 	# ----------------------------------------------------------------------------------------------
 	def __init__(self, obs_size: int, n_actions: int, hidden_size: int = 128):
 		"""
@@ -29,12 +34,61 @@ class DQN_net(torch.nn.Module):
 		self.net = torch.nn.Sequential(
 			torch.nn.Linear(obs_size, hidden_size),
 			torch.nn.ReLU(),
-			torch.nn.Linear(hidden_size, n_actions)
+			torch.nn.Linear(hidden_size, n_actions),
 		)
 
 	# ----------------------------------------------------------------------------------------------
 	def forward(self, x):
 		return self.net(x.float())
+
+
+# ==================================================================================================
+class Visual(torch.nn.Module):
+	# ----------------------------------------------------------------------------------------------
+	def __init__(self, input_shape, num_of_actions):
+		"""
+		Simple MLP network.
+
+		Args:
+			obs_size (int): Observation/state size of the environment
+			n_actions (int): Number of discrete actions available in the environment
+			hidden_size (int, optional): Size of hidden layers. Defaults to 128.
+		"""
+		super().__init__()
+
+		self.conv_nn = torch.nn.Sequential(
+			torch.nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+			torch.nn.BatchNorm2d(32),
+			torch.nn.ReLU(),
+			torch.nn.Conv2d(32, 64, kernel_size=4, stride=2),
+			torch.nn.BatchNorm2d(64),
+			torch.nn.ReLU(),
+			torch.nn.Conv2d(64, 64, kernel_size=3, stride=1),
+			torch.nn.BatchNorm2d(64),
+			torch.nn.ReLU(),
+		)
+		# Calculation of output of CNN so we can tell rest of NN what to expect on input.
+		# `input_shape` had to be 1 dimension lower because we dont know size of that dim upfornt
+		# So we need to add it every time if we want single frame to run through CNN
+		# np.prod flattens output by product of sizes of every dimension
+		cnn_output_shape = self.conv_nn(torch.zeros(1, *input_shape))
+		cnn_output_shape = int(np.prod(cnn_output_shape.size()))
+		# Output of regular NN will be 1x6 where 6 stands for 6 actions and how much NN thinks each action is right one
+		self.linear_nn = torch.nn.Sequential(
+			torch.nn.Linear(cnn_output_shape, 512),
+			torch.nn.ReLU(),
+			torch.nn.Linear(512, num_of_actions),
+		)
+
+	# ----------------------------------------------------------------------------------------------
+	def forward(self, x):
+		# Called with either one element to determine next action, or a batch
+		# during optimization. Returns tensor([[left0exp,right0exp]...]).
+
+		batch_size = x.size()[0] # Bacth size will be either 1 or BATCH_SIZE
+		# We need to flatten result of CNN and `view` reshapes tensor to have `batch_size` rows and data/batch_size columns (that is -1)
+		cnn_output = self.conv_nn(x).view(batch_size, -1)
+		return self.linear_nn(cnn_output) # apply rest of NN
 
 
 # ==================================================================================================
@@ -45,13 +99,25 @@ class DQN(Base):
 	# ----------------------------------------------------------------------------------------------
 	def __init__(self, args: argparse.Namespace, trial: Optional[optuna.trial.Trial] = None) -> None:
 		super().__init__(args, trial)
-		self.env = gym.make(self.hparams.env)
-		obs_size = self.env.observation_space.shape[0]
-		n_actions = self.env.action_space.n
-		self.example_input_array = torch.zeros(obs_size)
+		self.env = self.create_env()
 
-		self.net = DQN_net(obs_size, n_actions)
-		self.target_net = DQN_net(obs_size, n_actions)
+		if not hasattr(self.env, "_max_episode_steps"):
+			self.env._max_episode_steps = float("inf")
+
+		# FIX Discrete and Visual ModelTypes are not mutually exclusive
+		n_actions = self.env.action_space.n
+		if self.model_type is ModelType.DISCRETE:
+			obs_size = self.env.observation_space.shape[0]
+			self.net = Discrete(obs_size, n_actions)
+			self.target_net = Discrete(obs_size, n_actions)
+		elif self.model_type is ModelType.VISUAL:
+			obs_size = self.env.observation_space.shape
+			self.net = Visual(obs_size, n_actions)
+			self.target_net = Visual(obs_size, n_actions)
+		else:
+			raise NotImplementedError(self.model_type)
+		# self.example_input_array = torch.zeros(obs_size)
+		# self.example_input_array = torch.zeros([1, 210, 160, 3])
 
 		self.buffer = ReplayBuffer(self.hparams.replay_size)
 		self.agent = Agent(self.env, self.buffer)
@@ -91,7 +157,7 @@ class DQN(Base):
 		group.add_argument("--eps_end", type=float, default=0.01, help="Epsilon final value")
 		group.add_argument("--eps_last_frame", type=int, default=1000, help="Which frame epsilon should stop decaying at")
 
-		parser = RL.add_argparse_args(parser)
+		parser = RLOffPolicy.add_argparse_args(parser)
 
 		return parser
 
@@ -201,10 +267,11 @@ class DQN(Base):
 	def test_step(self, batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor], batch_idx: int) -> dict:
 		# Ignoring batch TODO don't even provide it to save time and GPU memory
 		self.agent.reset()
-		for i in range(self.env._max_episode_steps):
+		for i, _ in enumerate(iter(bool, True), start=1):
 			if i == 0:
 				self.trainer.progress_bar_callback.progress.reset(self.epoch_bar_id)
-			self.trainer.progress_bar_callback._update(self.epoch_bar_id, current=i, total=self.env._max_episode_steps)
+			# self.trainer.progress_bar_callback._update(self.epoch_bar_id, current=i, total=self.env._max_episode_steps)
+			self.trainer.progress_bar_callback._update(self.epoch_bar_id, current=i, total=float("inf"))
 			reward, done = self.agent.play_step(self.net, 0, self.device)
 			self.episode_reward += reward
 			if done:
@@ -212,7 +279,9 @@ class DQN(Base):
 				self.total_reward = self.episode_reward
 				self.episode_reward = 0
 
-			self.env.render()
+			if self.hparams.vis:
+				self.env.render()
+				time.sleep(1 / 30)
 			if done:
 				break
 
@@ -221,7 +290,8 @@ class DQN(Base):
 
 	# ----------------------------------------------------------------------------------------------
 	def on_test_end(self) -> None:
-		self.env.close()
+		if self.hparams.vis:
+			self.env.close()
 		return super().on_test_end()
 
 	# ----------------------------------------------------------------------------------------------
@@ -240,7 +310,7 @@ class DQN(Base):
 	def train_dataloader(self) -> DataLoader:
 		"""Initialize the Replay Buffer dataset used for retrieving experiences"""
 		return DataLoader(
-			RL(self.buffer, self.hparams.epoch_length),
+			RLOffPolicy(self.buffer, self.hparams.epoch_length),
 			batch_size=self.hparams.batch_size,
 			num_workers=self.hparams.workers,
 		)
@@ -256,7 +326,7 @@ class DQN(Base):
 	def test_dataloader(self) -> DataLoader:
 		"""Initialize the Replay Buffer dataset used for retrieving experiences"""
 		return DataLoader(
-			RL(self.buffer, self.hparams.epoch_length),
+			RLOffPolicy(self.buffer, self.hparams.epoch_length),
 			batch_size=1,
 			num_workers=self.hparams.workers,
 		)
