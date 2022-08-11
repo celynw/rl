1c992569d2af04c307d95ff4460a43b2928cc8d9
Celyn Walters <celyn.walters@surrey.ac.uk>
2022-08-11 19:28:55 +0100

    Add CartPole-events environment

--- rl/envs/__init__.py
+++ rl/envs/__init__.py
@@ -1,4 +1,6 @@
 from .cartpole_contrast import CartPoleEnvContrast
+from .cartpole_events import CartPoleEnvEvents
 from gym.envs.registration import register
 
 register(id="CartPole-contrast-v1", entry_point="rl.envs:CartPoleEnvContrast")
+register(id="CartPole-events-v1", entry_point="rl.envs:CartPoleEnvEvents")
