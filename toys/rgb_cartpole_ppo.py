5017c89fc3aed3733a65c999ac68f77300ebc964
Celyn Walters <celyn.walters@surrey.ac.uk>
2022-08-23 12:41:25 +0100

    Cleanup

--- toys/rgb_cartpole_ppo.py
+++ toys/rgb_cartpole_ppo.py
@@ -12,7 +12,6 @@ import torch
 import gym
 from stable_baselines3.common.monitor import Monitor
 # from gym.wrappers.monitoring.video_recorder import VideoRecorder
-
 import wandb
 from wandb.integration.sb3 import WandbCallback
 from tqdm import tqdm
@@ -55,37 +54,14 @@ def main(args: argparse.Namespace) -> None:
 		)
 
 	all_rewards = []
-	# env_ = gym.make(env_id)
-	# env_ = CartPoleRGBTemp(env_)
-	# env_ = DummyVecEnv([lambda: gym.make(env_id)])
-
 	env_ = gym.make(env_id)
-
-	# def make_env():
-	# 	return Monitor(env_, str(args.log_dir), allow_early_resets=True, info_keywords=("failReason", "updatedPolicy"))
-	# env = DummyVecEnv([make_env])
-	# env = VecVideoRecorder(env, f"videos/{run.id}", record_video_trigger=lambda x: x % 2000 == 0, video_length=200)
-
-	# while True:
-	# 	obs = env_.reset()
-	# print(obs)
-	# obs = np.transpose(obs, (1, 2, 0))
-	# import cv2
-	# cv2.imwrite(f"obs.png", obs)
-	# quit(0)
 
 	for i in tqdm(range(args.times)):
 		env = Monitor(env_, str(args.log_dir), allow_early_resets=True, info_keywords=("failReason", "updatedPolicy"))
 		policy_kwargs = dict(
 			# features_extractor_class=Estimator,
 			features_extractor_class=EDeNN,
-			# # # features_extractor_kwargs=dict(features_dim=128),
-			# # # features_extractor_kwargs=dict(features_dim=18),
-			# # features_extractor_kwargs=dict(features_dim=64),
-			# features_extractor_kwargs=dict(features_dim=4),
-			# net_arch: [{"pi": [64, 64], "vf": [64, 64]}] # DEFAULT
 			optimizer_class=torch.optim.Adam,
-			# optimizer_class=torch.optim.SGD,
 		)
 		model = PPO_mod(
 		# model = A2C_mod(
@@ -102,11 +78,6 @@ def main(args: argparse.Namespace) -> None:
 			# vf_coef=0.0,
 			# bs_coef=0.0,
 		) # total_timesteps will be at least n_steps (2048)
-		# model = PPO(MlpPolicy, env, verbose=1, learning_rate=args.lr) # total_timesteps will be at least n_steps (2048)
-		# inspect(model.policy.features_extractor, all=True)
-		# checkpoint = torch.load("/home/cw0071/dev/python/rl/toys/debug_training/runs/train_estimator/version_10/checkpoints/epoch=58-step=7375.ckpt")
-		# checkpoint = torch.load("/code/toys/debug_training/runs/train_estimator/version_10/checkpoints/epoch=58-step=7375.ckpt")
-		# checkpoint = torch.load("/home/cw0071/dev/python/rl/train_estimator_RL_estimator/219_1e4b47kx/checkpoints/epoch=59-step=75000.ckpt")
 
 		if args.load_feat:
 			assert model.policy.features_extractor is Estimator, "Only use --load_feat with `Estimator` feature extractor"
@@ -162,48 +133,22 @@ def main(args: argparse.Namespace) -> None:
 
 		env.set_model(model.policy.features_extractor)
 
-		# DEBUG observation space
-		# env_.reset()
-		# obs = env_.step(1)
-		# while obs[2] is False:
-		# 	obs = env_.step(1)
-		# print(np.unique(obs[0]))
-		# print(np.unique(obs[0], return_counts=True))
-		# quit(0)
-
-		# model = PPO(
-		# 	MlpPolicy,
-		# 	env,
-		# 	verbose=1,
-		# 	# Factor for trade-off of bias vs variance for Generalized Advantage Estimator.
-		# 	# Equivalent to classic advantage when set to 1.
-		# 	# https://arxiv.org/abs/1506.02438
-		# 	# To obtain Monte-Carlo advantage estimate
-		# 	# 	(A(s) = R - V(S))
-		# 	# where R is the sum of discounted reward with value bootstrap
-		# 	# (because we don't always have full episode), set ``gae_lambda=1.0`` during initialization
-		# 	# gae_lambda=1.0,
-		# 	# clip_range=0.0,
-		# 	# max_grad_norm=1.0,
-		# )
 		if use_wandb:
 			wandbCallback = WandbCallback(
 				model_save_path=args.log_dir / f"{args.name}",
 				gradient_save_freq=100,
 			)
-			callback = [TqdmCallback(), wandbCallback, PolicyUpdateCallback(env)]
+			callbacks = [TqdmCallback(), wandbCallback, PolicyUpdateCallback(env)]
 		else:
-			callback = [TqdmCallback(), PolicyUpdateCallback(env)]
+			callbacks = [TqdmCallback(), PolicyUpdateCallback(env)]
 		model.learn(
 			total_timesteps=max(model.n_steps, args.steps),
-			# callback=[TqdmCallback()],
-			callback=callback,
+			callback=callbacks,
 			eval_freq=args.n_steps,
 		)
 		all_rewards.append(env.get_episode_rewards())
 
 		if args.save and i == 0:
-			# model.save(args.log_dir / "a2c_cartpole_rgb")
 			model.save(args.log_dir / f"{args.name}")
 
 		if args.render and i == args.times - 1:
@@ -215,27 +160,7 @@ def main(args: argparse.Namespace) -> None:
 				if done:
 					obs = env.reset()
 	env.close()
-	# run.finish()
-
-	# all_rewards_ = -np.ones([len(all_rewards), max([len(x) for x in all_rewards])])
-	# for i, sub in enumerate(all_rewards):
-	# 	all_rewards_[i][0:len(sub)] = sub
-	# all_rewards = np.ma.MaskedArray(all_rewards_, mask=all_rewards_ < 0)
-
-	# mean = all_rewards.mean(axis=0)
-	# std = all_rewards.std(axis=0)
-	# ax = plt.gca()
-	# ax.set_ylim([0, 500]) # TODO get env max reward instead of hard coding
-	# ax.fill_between(
-	# 	range(all_rewards.shape[1]), # X
-	# 	# np.clip(mean - std, 0, None), # Max
-	# 	mean - std, # Max
-	# 	mean + std, # Min
-	# 	alpha=.5,
-	# 	linewidth=0
-	# )
-	# ax.plot(mean, linewidth=2)
-	# plt.savefig(str(args.log_dir / f"plot.png"))
+	# run.finish() # wandb?
 
 
 # ==================================================================================================
