982383a16f2e0d3f56a07b62f181d396d3f66e05
Celyn Walters <celyn.walters@surrey.ac.uk>
2022-09-13 00:18:25 +0100

    Remove `CartPoleEnvContrast`

--- rl/envs/__init__.py
+++ rl/envs/__init__.py
@@ -1,4 +1,3 @@
-from .cartpole_contrast import CartPoleEnvContrast
 from .cartpole_events import CartPoleEnvEvents
 from .cartpole_events_sim import CartPoleEnvEventsSim
 from .cartpole_events_debug import CartPoleEnvEventsDebug
@@ -6,7 +5,6 @@ from .cartpole_rgb import CartPoleEnvRGB
 from .mountaincar_events import MountainCarEnvEvents
 from gym.envs.registration import register
 
-register(id="CartPole-contrast-v1", entry_point="rl.envs:CartPoleEnvContrast")
 register(id="CartPole-events-v1", entry_point="rl.envs:CartPoleEnvEvents")
 register(id="CartPole-events-sim-v1", entry_point="rl.envs:CartPoleEnvEventsSim")
 register(id="CartPole-events-debug", entry_point="rl.envs:CartPoleEnvEventsDebug", max_episode_steps=10)
