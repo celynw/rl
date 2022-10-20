3a4906c4bd293f57374347247bc3ec74947dfa94
Celyn Walters <celyn.walters@surrey.ac.uk>
2022-10-20 14:14:52 +0100

    Add supporting link

--- rl/environments/pong.py
+++ rl/environments/pong.py
@@ -20,6 +20,7 @@ class PongEvents(AtariEnv):
 		# PongNoFrameskip-v4
 		# NOTE: I'm using a frameskip
 		# NOTE: They also use a frame stack of 4
+		# https://github.com/DLR-RM/rl-baselines3-zoo/blob/master/hyperparams/ppo.yml#L1
 		# https://github.com/DLR-RM/rl-trained-agents/blob/1e2a45e5d06efd6cc15da6cf2d1939d72dcbdf87/ppo/PongNoFrameskip-v4_1/PongNoFrameskip-v4/config.yml
 		# and refer to https://github.com/DLR-RM/rl-trained-agents/blob/1e2a45e5d06efd6cc15da6cf2d1939d72dcbdf87/ppo/PongNoFrameskip-v4_1/PongNoFrameskip-v4/args.yml
 		# # parser.set_defaults(steps=10000000)
