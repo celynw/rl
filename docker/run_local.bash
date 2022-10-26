441a0babd1f20625d376e42ac264137c1f1be2e7
Celyn Walters <celyn.walters@surrey.ac.uk>
2022-10-26 12:58:12 +0100

    Use existing env vars and terminfo

--- docker/run_local.bash
+++ docker/run_local.bash
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# -S -s 500000 -d /runs/ --projection_head --optuna postgresql://postgres:password@$HOSTNAME:5432/postgres"
-cmd="/code/venv/bin/python /code/train.py"
-WANDB_API_KEY="local-10f17eeb44c533e126f6639891d16595a1c7f337"
-WANDB_BASE_URL="http://umcvplws196:8080"
+cmd="/code/venv/bin/python /code/train.py" # -d /runs/ --optuna postgresql://postgres:password@$HOSTNAME:5432/postgres"
 
-id=$(docker run -tid --runtime=nvidia -P -v ~/dev/python/rl/venv:/home/docker/venv -v ~/dev/python/rl:/code -v ~/dev/python/rl/catkin_ws:/catkin_ws -v ~/dev/python/rl/runs:/runs -e WANDB_API_KEY=$WANDB_API_KEY -e WANDB_BASE_URL=$WANDB_BASE_URL celynw/esim_rl:v4)
+id=$(docker run -tid --runtime=nvidia -P -v ~/dev/python/rl/venv:/home/docker/venv -v ~/dev/python/rl:/code -v ~/dev/python/rl/catkin_ws:/catkin_ws -v ~/dev/python/rl/runs:/runs -e WANDB_BASE_URL -e WANDB_API_KEY -e TERM celynw/esim_rl:v5)
 echo "Docker image ID: $id"
 
+infocmp | docker exec -i $id tic -x -o /usr/lib/terminfo /dev/stdin # Allow user's terminfo for their current TERM
 docker exec -id $id bash -ci "roscore"
 docker exec -id $id bash -ci ". /catkin_ws/devel/setup.bash && roslaunch esim_ros --wait gym.launch config:=cfg/img.conf"
 # docker exec -ti $id bash -ci "/code/venv/bin/python -m pip install -e /code"
--- docker/run_project.bash
+++ docker/run_project.bash
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-# -S -s 500000 -d /runs/ --projection_head --optuna postgresql://postgres:password@$HOSTNAME:5432/postgres"
-cmd="/vol/research/reflexive3/rl/venv/bin/python /vol/research/reflexive3/rl/toys/rgb_cartpole_ppo.py"
+cmd="/vol/research/reflexive3/rl/venv/bin/python /vol/research/reflexive3/rl/train.py" # -d /runs/ --optuna postgresql://postgres:password@$HOSTNAME:5432/postgres"
 WANDB_API_KEY="local-10f17eeb44c533e126f6639891d16595a1c7f337"
 WANDB_BASE_URL="http://umcvplws196:8080"
 
-id=$(docker run -tid --runtime=nvidia -P -v /vol/research/reflexive3/rl/:/vol/research/reflexive3/rl/ -v /vol/research/reflexive3/rl/catkin_ws/:/vol/research/reflexive3/rl/catkin_ws/ -v /vol/research/reflexive3/rl/runs/:/vol/research/reflexive3/rl/runs/ -e WANDB_API_KEY=$WANDB_API_KEY -e WANDB_BASE_URL=$WANDB_BASE_URL celynw/esim_rl:v3)
+id=$(docker run -tid --runtime=nvidia -P -v /vol/research/reflexive3/rl/:/vol/research/reflexive3/rl/ -v /vol/research/reflexive3/rl/catkin_ws/:/vol/research/reflexive3/rl/catkin_ws/ -v /vol/research/reflexive3/rl/runs/:/vol/research/reflexive3/rl/runs/ -e WANDB_API_KEY -e WANDB_BASE_URL celynw/esim_rl:v5)
 echo "Docker image ID: $id"
 
+infocmp | docker exec -i $id tic -x -o /usr/lib/terminfo /dev/stdin # Allow user's terminfo for their current TERM
 docker exec -id $id bash -ci "roscore"
 docker exec -id $id bash -ci ". /vol/research/reflexive3/rl/catkin_ws/devel/setup.bash && roslaunch esim_ros --wait gym.launch config:=cfg/img.conf"
 docker exec -i $id bash -ci "/vol/research/reflexive3/rl/venv/bin/python -m pip install -e /vol/research/reflexive3/rl"
