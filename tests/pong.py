ac145effc643ac15c073364df6cdc687ae8c1386
Celyn Walters <celyn.walters@surrey.ac.uk>
2023-01-25 03:26:27 +0000

    Remove bad import

--- tests/pong.py
+++ tests/pong.py
@@ -6,7 +6,6 @@ from pathlib import Path
 # import envpool
 import wandb
 from wandb.integration.sb3 import WandbCallback
-from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack, VecTransposeImage#, VecVideoRecorder
 from stable_baselines3.common.callbacks import CheckpointCallback
